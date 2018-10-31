da26d61f5a6aee3a3c4b5f97fabeba470062d633
吴佰川 <baichuan.wu@ucarinc.com>
2018-10-31 15:25:21 +0800

    [UPDATE]

--- src/main/java/com/uc/training/smadmin/demo/vo/DemoTestVO.java
+++ src/main/java/com/uc/training/smadmin/demo/vo/DemoTestVO.java
@@ -21,7 +21,7 @@ public class DemoTestVO extends PageQuery {
     @NotNull(message = "性别不能为空")
     private Integer sex;
     @NotBlank(message = "手机号不能为空")
-    @Pattern(regexp = "^1[3456789]\\d{9}$", message = "手机号格式不正确")
+    @Pattern(regexp = "^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\\d{8}$", message = "手机号格式不正确")
     private String cellphone;
 
     public String getName() {
