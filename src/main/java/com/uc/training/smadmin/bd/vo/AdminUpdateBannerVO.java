cba01e74f1520a2a0bf4d55ec92c7f665be6fc2a
hq1022 <2272808281@qq.com>
2018-11-01 17:15:11 +0800

    [UPDATE]后台广告

---
+++ src/main/java/com/uc/training/smadmin/bd/vo/AdminUpdateBannerVO.java
@@ -0,0 +1,33 @@
+package com.uc.training.smadmin.bd.vo;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * 版权声明： Copyright (c) 2008 ucarinc. All Rights Reserved.
+ *
+ * @author 何麒（qi.he@ucarinc.com）
+ * @Version 1.0
+ * @date 2018/11/1
+ */
+public class AdminUpdateBannerVO extends AdminInsertBannerVO {
+    /**
+     * 主键id
+     */
+    @NotNull(message = "请重新修改")
+    private Long id;
+
+    @Override
+    public String toString() {
+        return "AdminUpdateBannerVO{" +
+                "id=" + id +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
