3df6ff8ca7f3dbef290bd6e519fe2deb9d34ce9e
vino <710045538@qq.com>
2018-11-05 10:28:11 +0800

    更新了消息功能

--- src/main/java/com/uc/training/smadmin/bd/mq/GrowthMqConsumer.java
+++ src/main/java/com/uc/training/smadmin/bd/mq/GrowthMqConsumer.java
@@ -1,22 +1,14 @@
 package com.uc.training.smadmin.bd.mq;
 
 import com.alibaba.fastjson.JSON;
-import com.uc.training.common.enums.ConsumerTypeEnum;
 import com.uc.training.smadmin.bd.service.GrowthDetailService;
-import com.uc.training.smadmin.bd.service.impl.GrowthDetailServiceImpl;
 import com.uc.training.smadmin.bd.vo.GrowthVO;
-import com.uc.training.smadmin.bd.vo.LoginMqVO;
 import com.uc.training.smadmin.bd.vo.MqVO;
 import com.uc.training.smadmin.utils.InjectionUtils;
-import com.zuche.framework.common.SpringApplicationContext;
 import com.zuche.framework.metaq.handler.DefaultExecutorMessageListener;
 import com.zuche.framework.metaq.vo.MessageVO;
-import com.zuche.framework.remote.nio.codec.HessianSerializerUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-
-import java.util.Map;
 
 /**
  * 版权说明：Copyright (c) 2018 ucarinc. All Rights Reserved.
@@ -36,10 +28,11 @@ public class GrowthMqConsumer extends DefaultExecutorMessageListener {
         this.growthDetailService = InjectionUtils.getInjectionInstance(GrowthDetailService.class);
         MqVO mqVO = JSON.parseObject(message.getData(), MqVO.class);
         //判断消费类型是否为成长值
-        if (mqVO.getConsumerType().equals(ConsumerTypeEnum.GROWTHTYPE.getConsumerType())){
+        if (mqVO.getGrowthType() != null){
             GrowthVO growthVO = new GrowthVO();
             growthVO.setMemberId(mqVO.getMemberId());
             growthVO.setGrowthType(mqVO.getGrowthType());
+            growthVO.setPurchaseValue(mqVO.getPurchaseValue());
             growthDetailService.saveGrowthDetail(growthVO);
         }
     }
