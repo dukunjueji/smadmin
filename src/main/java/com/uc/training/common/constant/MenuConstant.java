a692a12fed1ecc84658d9edb48aab41dcad56f7a
吴佰川 <baichuan.wu@ucarinc.com>
2018-11-08 12:24:46 +0800

    [UPDATE]bug fixed

--- src/main/java/com/uc/training/common/constant/MenuConstant.java
+++ src/main/java/com/uc/training/common/constant/MenuConstant.java
@@ -5,29 +5,31 @@ package com.uc.training.common.constant;
  * @Date: 2018/11/6 13:41
  * @Description: 菜单常量
  */
-public class MenuConstant {
+public final class MenuConstant {
+    private MenuConstant() {
+    }
     /**
      * 菜单名最大长度
      */
-    public static int LONGEST_NAME_LENGTH = 32;
+    public static final int LONGEST_NAME_LENGTH = 32;
 
     /**
      * 链接地址最大长度
      */
-    public static int LONGEST_URL_LENGTH = 255;
+    public static final int LONGEST_URL_LENGTH = 255;
 
     /**
      * 标识最大长度
      */
-    public static int LONGEST_REL_LENGTH = 32;
+    public static final int LONGEST_REL_LENGTH = 32;
 
     /**
      * 最小排序
      */
-    public static int MIN_SORT = 0;
+    public static final int MIN_SORT = 0;
 
     /**
      * 最大排序
      */
-    public static int MAX_SORT = 1000;
+    public static final int MAX_SORT = 1000;
 }
