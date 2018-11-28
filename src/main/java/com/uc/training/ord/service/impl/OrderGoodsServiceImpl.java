4f59f18698d12f85d5451fec749e622ab47e8c05
DK <494173758@qq.com>
2018-11-28 18:46:19 +0800

    完善Rpc服务

---
+++ src/main/java/com/uc/training/ord/service/impl/OrderGoodsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.uc.training.ord.service.impl;
+
+import com.uc.training.smadmin.ord.dao.OrderGoodsDao;
+import com.uc.training.smadmin.ord.model.Order;
+import com.uc.training.smadmin.ord.service.OrderGoodsService;
+import com.uc.training.smadmin.ord.vo.OrdOrderGoodsVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 版权声明： Copyright (c) 2008 ucarinc. All Rights Reserved.
+ *
+ * @author 何麒（qi.he@ucarinc.com）
+ * @Version 1.0
+ * @date 2018/11/8
+ */
+@Service
+public class OrderGoodsServiceImpl implements OrderGoodsService {
+
+}
