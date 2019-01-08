786cac3467aca5cad634cd372c996eaab4e93ee2
DK <494173758@qq.com>
2019-01-08 10:38:15 +0800

    更新文件

---
+++ src/main/java/com/uc/training/SmadminApplication.java
@@ -0,0 +1,29 @@
+package com.uc.training;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**  
+ * 测试消费者启动器
+ *     
+ * @author wubc
+ * @date 2018年12月13日 下午2:01:05   
+ * @version V1.0 
+ */  
+@EnableFeignClients
+@EnableEurekaClient
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SmadminApplication {
+	private static final Logger logger = LoggerFactory.getLogger(SmadminApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmadminApplication.class, args);
+		logger.info("==================== micro-smadmin 启动成功!====================");
+	}
+}
