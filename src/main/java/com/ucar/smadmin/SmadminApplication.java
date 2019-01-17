577ee7ca20afe26d4c59b0c9c9cc53d08a7e7209
DK <494173758@qq.com>
2019-01-17 17:50:12 +0800

    更新

--- src/main/java/com/ucar/smadmin/SmadminApplication.java
+++ src/main/java/com/ucar/smadmin/SmadminApplication.java
@@ -1,18 +1,15 @@
 package com.ucar.smadmin;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-
-import javax.sql.DataSource;
-
 /**
  * 测试消费者启动器
  *
@@ -31,25 +28,4 @@ public class SmadminApplication {
         SpringApplication.run(SmadminApplication.class, args);
         logger.info("==================== micro-smadmin 启动成功!====================");
     }
-
-//    @Autowired
-//    private Environment env;
-//
-//    @Bean
-//    public DataSource dataSource() {
-//        DruidDataSource dataSource = new DruidDataSource();
-//        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
-//        dataSource.setUrl(env.getProperty("spring.datasource.url"));
-//        dataSource.setUsername(env.getProperty("spring.datasource.username"));
-//        dataSource.setPassword(env.getProperty("spring.datasource.password"));
-//        dataSource.setInitialSize(10);
-//        dataSource.setMaxActive(50);
-//        dataSource.setMinIdle(0);
-//        dataSource.setMaxWait(60000);
-//        dataSource.setValidationQuery("SELECT 1");
-//        dataSource.setTestOnBorrow(false);
-//        dataSource.setTestWhileIdle(true);
-//        dataSource.setPoolPreparedStatements(false);
-//        return dataSource;
-//    }
 }
